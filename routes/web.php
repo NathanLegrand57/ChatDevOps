c680caf8a90ef735a465e534464cfdf54049901f
Mitroglou <nathanlgd14@gmail.com>
2023-11-24 16:44:37 +0100

    Changement de nom du fichier .JENKINS

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,6 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::get('/', [ChatController::class, 'index']);
+Route::get('/', [ChatController::class, 'index']); //qdiuhqdiuhaziuhdau
 
 Route::resource('/chat', ChatController::class);
